ccbea1be72c52db84a1d9deb9d4082512e2f668c
c.san <80271352+csanry@users.noreply.github.com>
2024-05-10 08:42:26 +0800

    chore: add setup_dwh script

---
+++ dags/dbt/tpch-pipeline/scripts/setup_dwh.sql
@@ -0,0 +1,11 @@
+-- setup wh, db and role
+use role accountadmin;
+create warehouse if not exists dbt_wh with warehouse_size='x-small';
+create database if not exists dbt_db;
+create role if not exists dbt_role;
+create schema if not exists dbt_db.dbt_schema;
+
+-- extend grants to user
+grant usage on warehouse dbt_wh to role dbt_role;
+grant all on database dbt_db to role dbt_role;
+grant role dbt_role to user csanry;
